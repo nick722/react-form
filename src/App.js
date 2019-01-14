aebbec4f808c8889a099476cfd29a81dbca53ae1
nick722 <79ZUdtd3orzC!GeTcb^s&79jBB>
2019-01-14 22:01:48 +0300

    Add FavoriteColorForm component fomr React Essential Training

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import FavoriteColorForm from "./components/FavoriteColorForm";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         YN React Form
+        <FavoriteColorForm />
       </div>
     );
   }
